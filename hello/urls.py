3d22da02def3c5c299ff223362ec9c2b01178edd
vinayak pratap <104990878+croshado@users.noreply.github.com>
2022-06-01 13:19:51 +0530

    Update urls.py

--- hello/urls.py
+++ hello/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path as url
 from django.conf import settings
 from django.conf.urls.static import static
 
